c053a8776ff9e04cdb40dd2366b9de5c7e300264
jjmertes <jjmertes@wisc.edu>
2026-03-01 15:44:34 -0600

    changes to backend files so they can run now

--- backend/src/main/java/com/university/backend/FlightController.java
+++ backend/src/main/java/com/university/backend/FlightController.java
@@ -1,52 +1,38 @@
 package com.university.backend;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/flights")
 public class FlightController {
-    
-    /*
-     * Handles flightService GET
-    */
-    @GetMapping("/search")
-    public ResponseEntity<String> search(@RequestParameter String flightNumber) {
 
+    @GetMapping("/search")
+    public ResponseEntity<String> search(@RequestParam(required = false) String flightNumber) {
         return ResponseEntity.ok("Implement /search. FlightNumber: " + flightNumber);
     }
 
     @GetMapping("/{flightId}")
-    public ResponseEntity<String> getByFlightID(@PathVariable int flightID) {
+    public ResponseEntity<String> getByFlightID(@PathVariable String flightId) {
         return ResponseEntity.ok("Implement /{flightId}");
     }
 
     @GetMapping("/{flightId}/details")
-    public ResponseEntity<String> getDetails(@PathVariable int flightID) {
+    public ResponseEntity<String> getDetails(@PathVariable String flightId) {
         return ResponseEntity.ok("Implement /{flightId}/details");
     }
 
     @GetMapping("/active")
     public ResponseEntity<String> getActive() {
-        // return all active flights
         return ResponseEntity.ok("Implement /active. Return all active flights");
     }
 
     @GetMapping("/{flightId}/positions")
-    public ResponseEntity<String> getPositions(@PathVariable int flightID) {
+    public ResponseEntity<String> getPositions(@PathVariable String flightId) {
         return ResponseEntity.ok("Implement /{flightId}/positions");
     }
-
-}
-
-private enum Type {FLIGHT, AIRCRAFT, ENTITY}
-
-private static class Flight {
-	private int trackingID = 3;
-	private Type type = Type.FLIGHT;
-	private String flightNum;
-	private boolean notif;
-	private LocalDateTime time;
 }
