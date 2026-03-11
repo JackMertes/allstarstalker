70872efa0e7538365326c1c42ea8bd72cb1385dc
jjmertes <jjmertes@wisc.edu>
2026-03-10 23:11:49 -0500

    converts api to database information. endpoint now working to refresh one team

--- backend/src/test/java/com/university/backend/FlightRepositoryTest.java
+++ backend/src/test/java/com/university/backend/FlightRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Transactional
 class FlightRepositoryTest {
 
     @Autowired
