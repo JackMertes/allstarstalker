bd263de04708bc607d79f954e5dbc63ce01d56c4
jjmertes <jjmertes@wisc.edu>
2026-04-22 13:54:50 -0500

    more changes

--- backend/src/main/java/com/university/backend/WebConfig.java
+++ backend/src/main/java/com/university/backend/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://jackmertes.github.io"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
